55f7a67513ead6f8f451bf5e3e8f6b28fc113b2d
eaglehuntt <eagle@eaglehunt.net>
2023-09-28 16:05:45 -0700

    Updated nature to have correct multiplier. Now full stats are calculated correctly

--- src/modules/pokemon.py
+++ src/modules/pokemon.py
@@ -1,22 +1,36 @@
 import requests as rq
 import random
+import math
+
+# test: https://pycosites.com/pkmn/stat.php
+
+
+class IncorrectNatureError(Exception):
+    pass
 
 
 class Pokemon:
 
-    def __init__(self, pokedex_number):
-        self.data = self.get_data(pokedex_number)
-        self.type = self.get_type()
-        self.nature = self.get_nature()  # (name, (increased stat, decreased stat))
-        self.stats = self.get_stats()
+    def __init__(self, pokedex_number, nature="random"):
+        self.data = self._get_data(pokedex_number)
+        self.level = 50
+        self.type = self._get_type()
 
-    def get_data(self, pokedex_number):
+        # (name, (increased stat, decreased stat))
+        self.nature_name, self.nature_effects = self._get_nature(nature)
+
+        self.base_stats = self._get_base_stats()
+        self.ivs = self._get_ivs()
+        self.evs = self._get_evs()
+        self.total_stats = self._get_total_stats()
+
+    def _get_data(self, pokedex_number):
         pokemon_data = rq.get(
             f"https://pokeapi.co/api/v2/pokemon/{pokedex_number}").json()
 
         return pokemon_data
 
-    def get_type(self):
+    def _get_type(self):
         # Logic to check if pokemon is single or dual type
         if len(self.data["types"]) > 1:
             type1 = self.data["types"][0]["type"]["name"]
@@ -27,7 +41,7 @@ class Pokemon:
 
         return pokemon_type
 
-    def get_nature(self):
+    def _get_nature(self, nature):
 
         # data from https://www.reddit.com/r/PokemonFireRed/comments/kqj010/nature_chart/
 
@@ -57,12 +71,17 @@ class Pokemon:
             "careful": ("special-defense", "special-attack"),
         }
 
-        selected_nature = random.choice(list(natures.keys()))
-        stat_effect = natures[selected_nature]
+        if nature == "random":
+            nature = random.choice(list(natures.keys()))
 
-        return (selected_nature, stat_effect)
+        elif nature != "random" and nature not in list(natures.keys()):
+            raise IncorrectNatureError("The nature provided is not valid")
 
-    def get_stats(self):
+        stat_effect = natures[nature]
+
+        return (nature, stat_effect)
+
+    def _get_base_stats(self):
         # Logic to get base stat data and return it as a dictionary object
         stats_info = self.data["stats"]
         base_stats = {}
@@ -70,12 +89,82 @@ class Pokemon:
         for stat_object in stats_info:
             base_stats[stat_object["stat"]["name"]] = stat_object["base_stat"]
 
-        # apply nature to stats
-        nature_effect = self.nature[1]  # (increased stat, decreased stat)
-        base_stats[nature_effect[0]] += 10  # increase
-        base_stats[nature_effect[1]] -= 10  # decrease
-
         return base_stats
+
+    def _get_ivs(self):
+        ivs = {}
+
+        for stat in self.base_stats.keys():
+            value = random.randint(0, 31)
+            ivs[stat] = value
+
+        return ivs
+
+    def _get_evs(self):
+        evs = {}
+
+        # maximum total evs = 510
+        maximum_evs_remaining = random.randint(0, 510)
+        maximum_evs_per_stat = 252
+
+        # Initialize all EVs to 0
+        for stat in self.base_stats.keys():
+            evs[stat] = 0
+
+        while maximum_evs_remaining > 0:
+            selected_stat = random.choice(list(evs.keys()))
+
+            # Calculate the maximum EVs that can be added to this stat
+            remaining_evs_for_stat = maximum_evs_per_stat - evs[selected_stat]
+
+            if remaining_evs_for_stat > 0:
+                # Distribute random EVs (up to the remaining limit) to the selected stat
+                added_evs = random.randint(
+                    0, min(remaining_evs_for_stat, maximum_evs_remaining))
+                evs[selected_stat] += added_evs
+                maximum_evs_remaining -= added_evs
+
+        return evs
+
+    def _get_total_stats(self):
+        print(f"IVs: {self.ivs}")
+        print(f"EVs: {self.evs}")
+        print(f"Base Stats: {self.base_stats}")
+        print(f"Nature: {self.nature_name}")
+
+        # Calculation based on Generation III onward formula.
+        # https://pokemon.fandom.com/wiki/Statistics#
+
+        stats = {}
+
+        for stat in self.base_stats.keys():
+            base_stat = self.base_stats[stat]
+            iv = self.ivs[stat]
+            ev = self.evs[stat]
+            level = self.level
+
+            if stat == "hp":
+                hp_stat = math.floor(((2 * base_stat + iv + (ev // 4))
+                                      * level // 100) + level + 10)
+                stats["hp"] = hp_stat
+            else:
+                other_stat = math.floor((((2 * base_stat + iv + (ev // 4)) *
+                                          level // 100) + 5) * self.nature_multiplier(stat))
+                stats[stat] = other_stat
+
+        print(stats)
+
+        return stats
+
+    def nature_multiplier(self, stat):
+        increased_stat, decreased_stat = self.nature_effects
+
+        if stat == increased_stat:
+            return 1.1
+        elif stat == decreased_stat:
+            return 0.9
+        else:
+            return 1.0
 
 
 pkm = Pokemon(1)
